3546b051fc86354b4ec76109051fa380c28ecd64
WoodMan <85753549+nordbearbotdev@users.noreply.github.com>
2022-03-20 10:50:19 +0300

    Update ModGenerator.py

--- ModGenerator.py
+++ ModGenerator.py
@@ -26,3 +26,34 @@ def coderun():
     erx = None
     erc = None
     erv = None
+
+cred()
+    try:
+        Mn = raw_input("Напишите название мода (Без пробелов и специальных символов!): ")
+        Mn = Mn.translate(None, ' \\/":.,><`~!@#$%^&?;*+=')
+        if not Mn:
+            raise ValueError
+        else:
+            z = 1
+    except ValueError:
+        erz = "Название мода"
+    
+    try:
+        Md = raw_input("Напишите имя мода: ")
+        if not Md:
+            raise ValueError
+        else:
+            x = 1
+    except ValueError:
+        erx = "Имя мода"
+
+    try:
+        It = raw_input("Название первого предмета (Без пробелов и специальных символов!): ")
+        It = It.translate(None, ' \\/":.,><`~!@#$%^&?;*+=')
+        if not It:
+            raise ValueError
+        else:
+            c = 1
+    except ValueError:
+        erc = "Название первого предмета+"
+    
